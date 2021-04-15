dc82b43ffcca6e26ca6ecb6f872940844c8a6c43
marci <marcianszlovak@gmail.com>
2021-04-15 20:55:55 +0200

    [fix] disable logger in production mode

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
+import { LoggerService } from './core/core/logger.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+    if (environment.production) {
+      LoggerService.enableProductionMode();
+    }
+  }
+}
